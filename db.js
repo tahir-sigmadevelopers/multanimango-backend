d2ced4acb02af2ff557bdd89836d8bde71c0b00a
Mohammad Tahir <starghareeb@gmail.com>
2025-08-25 02:47:48 +0500

    bckend bhi completed hai

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async()=>{
     
     try {
-      await mongoose.connect("mongodb://localhost:27017/MangooseKaData")
+      await mongoose.connect("mongodb://tahirsultan:_isHacked1@cluster0-shard-00-00.z2sch.mongodb.net:27017,cluster0-shard-00-01.z2sch.mongodb.net:27017,cluster0-shard-00-02.z2sch.mongodb.net:27017/MultaniMango?ssl=true&replicaSet=atlas-ranagn-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0")
     console.log("database is connected");  
     } catch (error) {
         console.log(error);
