926298878621ecf58086d616865b22a283510c53
Mohammad Tahir <starghareeb@gmail.com>
2025-08-25 03:08:32 +0500

    frontend url added

--- Sever.js
+++ Sever.js
@@ -22,7 +22,7 @@ cloudinary.config({
 dotenv.config()
 
 app.use(cors({
-    origin:["http://localhost:5174","http://localhost:5173"]
+    origin:["http://localhost:5174","http://localhost:5173", "https://multanimango.vercel.app/"]
 }))
 
 app.use(express.json({limit:"50mb"}))
